7281067886499b9b1e448c19b79509408fae9f6b
XtremeSakib <codewithraihan@gmail.com>
2026-06-20 16:19:26 +0600

    addedPostal

--- ContactManagement/Model/Contact.cs
+++ ContactManagement/Model/Contact.cs
@@ -8,6 +8,6 @@
         public string? Phone { get; set; }
         public string? Address { get; set; }
 
-        public string? City { get; set; }
+        public string? Postal { get; set; }
     }
 }
