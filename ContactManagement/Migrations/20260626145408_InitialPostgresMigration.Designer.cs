c57288363214e51cc318200a6025f57403e239c0
XtremeSakib <codewithraihan@gmail.com>
2026-06-26 21:51:47 +0600

    git update

--- ContactManagement/Migrations/20260626145408_InitialPostgresMigration.Designer.cs
+++ ContactManagement/Migrations/20260626145408_InitialPostgresMigration.Designer.cs
@@ -3,17 +3,17 @@ using System;
 using ContactManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
 namespace ContactManagement.Migrations
 {
     [DbContext(typeof(ContactManagementDBContext))]
-    [Migration("20260625121530_InitialContactSetup")]
-    partial class InitialContactSetup
+    [Migration("20260626145408_InitialPostgresMigration")]
+    partial class InitialPostgresMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -21,35 +21,36 @@ namespace ContactManagement.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "9.0.17")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+                .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+            NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
             modelBuilder.Entity("ContactManagement.Model.Contact", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+                        .HasColumnType("integer");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
                     b.Property<string>("Address")
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("text");
 
                     b.Property<int>("CreatedBy")
-                        .HasColumnType("int");
+                        .HasColumnType("integer");
 
                     b.Property<DateTime>("CreatedDate")
-                        .HasColumnType("datetime2");
+                        .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("Email")
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("text");
 
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasColumnType("text");
 
                     b.Property<string>("Phone")
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("text");
 
                     b.HasKey("Id");
 
--- ContactManagement/Program.cs
+++ ContactManagement/Program.cs
@@ -7,10 +7,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. এখানে আমরা শুধুমাত্র একটি শক্তিশালী CORS পলিসি রাখলাম (AllowAll)
+// 1. CORS পলিসি সার্ভিস যোগ করা (Vercel ফ্রন্টএন্ডের জন্য)
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("AllowVercelApp", policy =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
@@ -18,30 +18,31 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+// 2. PostgreSQL ডাটাবেজ কনটেক্সট রেজিস্টার করা
 builder.Services.AddDbContext<ContactManagementDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ContactManagementConnectionString")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ContactManagementConnectionString")));
 
+// 3. ডিপেনডেন্সি ইনজেকশন (Dependency Injection) রেজিস্টার করা
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 builder.Services.AddScoped<IContactService, ContactService>();
 builder.Services.AddScoped<IUnitofWork, UnitofWork>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// 4. CORS মিডলওয়্যার (অবশ্যই builder.Build() এর নিচে এবং UseAuthorization এর উপরে)
+app.UseCors("AllowVercelApp");
+
+// প্রোডাকশনেও যেন সোয়াগার বা এপিআই রেসপন্স ঠিকঠাক পাওয়া যায়, তাই শর্তটি কিছুটা শিথিল রাখা হলো
+if (app.Environment.IsDevelopment() || true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-// 2. ঠিক এই পজিশনে (Routing/Redirection এর পর এবং Authorization এর আগে) CORS কল করতে হয়
-app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
